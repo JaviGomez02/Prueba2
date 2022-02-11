d8ea6049eb0c25ee8f839ca1af5ad44fd87b39eb
estudiante <javigomez055@gmail.com>
2022-02-11 13:42:08 +0100

    committtt

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -3,7 +3,7 @@ package main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		//hola amigos
+		//hola amigos esto es un conflicto
 		System.out.println("Hola mundo");
 		
 	}
